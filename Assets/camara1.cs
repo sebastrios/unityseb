cb5c36d549883d1c9714d4febd1f9c69011aba67
Sala de Informática 6 <Sala6@academia.usbctg.local>
2023-09-01 12:33:37 -0500

    cambios iniciales

---
+++ Assets/camara1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Cube");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y +5, player.transform.position.z - 10);
+
+    }
+}
